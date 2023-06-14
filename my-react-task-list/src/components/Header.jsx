0c6a85c90833638090ba36987375bd5426a38237
Andres castañeda <estiven.casta12@gmail.com>
2023-06-14 08:07:48 -0500

    se agrega el titulo en header

---
+++ my-react-task-list/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Mi lista de tareas</h1>
+        </header>
+    );
+};
+
+export default Header;
