e4129f715e3f31a29c18ea18c5bb8f7185b13513
Andres castañeda <estiven.casta12@gmail.com>
2023-06-17 22:55:08 -0500

    se crea lista de tareas

--- my-react-task-list/src/components/TaskList.jsx
+++ my-react-task-list/src/components/TaskList.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import Task from './Task';
 
-const TaskList = () => {
-    const tasks = [
-        {id: 1, name: 'Task 1', completed: false},
-        {id: 2, name: 'Task 2', completed: true},
-        {id: 3, name: 'Task 3', completed: false},
-    ];
+function TaskList() {
+  const tasks = [
+    { id: 1, name: 'Task 1', completed: false },
+    { id: 2, name: 'Task 2', completed: true },
+    { id: 3, name: 'Task 3', completed: false },
+  ];
 
-    retunr (
-        <div>
-            <h2>Lista de tareas</h2>
-            <ul>
-                {tasks.map( (task) => (
-                    <task key={task.id} task={task}/>
-                ))}
-            </ul>
-        </div>
-    );
-};
+  return (
+    <div>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
 
 export default TaskList;
