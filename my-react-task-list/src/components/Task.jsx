56495b0707051ecea7f9c96a8087c707d31b37b7
Andres castañeda <estiven.casta12@gmail.com>
2023-06-22 09:45:46 -0500

    Se agrega react forms

--- my-react-task-list/src/components/Task.jsx
+++ my-react-task-list/src/components/Task.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
 function Task({ task, handleTaskToggle, handleTaskDelete, handleTaskUpdate }) {
-  const { id, name, completed } = task;
+  const { id, name, description, completed } = task;
   const [isEditing, setIsEditing] = useState(false);
   const [updatedName, setUpdatedName] = useState(name);
+  const [updatedDescription, setUpdatedDescription] = useState(description);
 
   const handleToggle = () => {
     handleTaskToggle(id);
@@ -13,35 +14,46 @@ function Task({ task, handleTaskToggle, handleTaskDelete, handleTaskUpdate }) {
     handleTaskDelete(id);
   };
 
-  const handleEdit = () => {
-    setIsEditing(true);
+  const handleToggleEdit = () => {
+    setIsEditing(!isEditing);
   };
 
   const handleUpdate = () => {
-    if (updatedName.trim() !== "") {
-      handleTaskUpdate(id, { name: updatedName, completed });
+    if (updatedName.trim().length >= 3) {
+      handleTaskUpdate(id, {
+        name: updatedName,
+        description: updatedDescription,
+        completed,
+      });
       setIsEditing(false);
+    } else {
+      alert("El nombre de la tarea debe tener al menos 3 caracteres.");
     }
   };
 
   return (
     <div>
       {isEditing ? (
-        <>
+        <div>
           <input
             type="text"
             value={updatedName}
             onChange={(e) => setUpdatedName(e.target.value)}
           />
+          <textarea
+            value={updatedDescription}
+            onChange={(e) => setUpdatedDescription(e.target.value)}
+          ></textarea>
           <button onClick={handleUpdate}>Actualizar</button>
-        </>
+        </div>
       ) : (
-        <>
-          <span>{name}</span>
+        <div>
+          <h3>{name}</h3>
+          <p>{description}</p>
           <input type="checkbox" checked={completed} onChange={handleToggle} />
-          <button onClick={handleEdit}>Actualizar</button>
+          <button onClick={handleToggleEdit}>Actualizar</button>
           <button onClick={handleDelete}>Eliminar</button>
-        </>
+        </div>
       )}
     </div>
   );
--- my-react-task-list/src/components/TaskList.jsx
+++ my-react-task-list/src/components/TaskList.jsx
@@ -5,6 +5,8 @@ import useTask from "../hooks/useTask";
 function TaskList() {
   const { tasks, createTask, deleteTask, updateTask } = useTask();
   const [newTaskName, setNewTaskName] = useState("");
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+  const [editingTaskId, setEditingTaskId] = useState(null);
 
   const handleTaskToggle = (id) => {
     const updatedTask = tasks.find((task) => task.id === id);
@@ -17,40 +19,62 @@ function TaskList() {
   };
 
   const handleAddTask = () => {
-    if (newTaskName.trim() !== "") {
+    if (newTaskName.trim().length >= 3) {
       const newTask = {
         id: Date.now(),
         name: newTaskName,
+        description: newTaskDescription,
         completed: false,
       };
       createTask(newTask);
       setNewTaskName("");
+      setNewTaskDescription("");
+    } else {
+      alert("El nombre de la tarea debe tener al menos 3 caracteres.");
     }
+  };
+
+  const handleTaskEdit = (id) => {
+    setEditingTaskId(id);
   };
 
   const handleTaskUpdate = (id, updatedTask) => {
     updateTask(id, updatedTask);
+    setEditingTaskId(null);
   };
 
   return (
     <div>
-      <div>
+      <div style={{ marginBottom: "1rem" }}>
         <input
           type="text"
+          placeholder="Nombre de la tarea"
           value={newTaskName}
           onChange={(e) => setNewTaskName(e.target.value)}
+          style={{ marginRight: "1rem" }}
         />
+        <textarea
+          placeholder="Descripción de la tarea"
+          value={newTaskDescription}
+          onChange={(e) => setNewTaskDescription(e.target.value)}
+          style={{ marginBottom: "1rem" }}
+        ></textarea>
         <button onClick={handleAddTask}>Agregar Tarea</button>
       </div>
-      {tasks.map((task) => (
-        <Task
-          key={task.id}
-          task={task}
-          handleTaskToggle={handleTaskToggle}
-          handleTaskDelete={handleTaskDelete}
-          handleTaskUpdate={handleTaskUpdate}
-        />
-      ))}
+
+      <div>
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            isEditing={editingTaskId === task.id}
+            handleTaskToggle={handleTaskToggle}
+            handleTaskDelete={handleTaskDelete}
+            handleTaskEdit={handleTaskEdit}
+            handleTaskUpdate={handleTaskUpdate}
+          />
+        ))}
+      </div>
     </div>
   );
 }
